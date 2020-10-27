ca6312693d92d411bff52c20a6cc738380f5da9d
frendhisaido <frendhi.saido@outlook.com>
2020-10-27 18:01:24 +0700

    change to 1000 iterations

--- puller.js
+++ puller.js
@@ -10,21 +10,26 @@ var pusher_address = `tcp://127.0.0.1:${bind_port}`;
 console.log(`Puller connected to ${pusher_address}`);
 
 puller.on("message", function(message) {
-    var json_message = JSON.parse(message.toString())
-    var proc_filename = './out/file_'+json_message.num+'.log';
-    console.log(json_message.num)
-    try {
-        if (fs.existsSync(proc_filename)) {
-            fs.unlinkSync(proc_filename);
-            console.log(`${bind_port} Removed : ${proc_filename}`);
-        }else{
-            fs.closeSync(fs.openSync(proc_filename, 'w'));
-            console.error(`${bind_port} Creating : ${proc_filename}`);
-        }
-      } catch(err) {
-        console.error(`Error : ${proc_filename}`);
-      }
-    
+    const json_message = JSON.parse(message.toString())
+    let filepath = './out/file_'+json_message.num+'.log';
+    console.log(json_message.num);
+    process_task(filepath)
 });
+
+function process_task(filepath) {
+    setTimeout(function(){ 
+        try {
+            if (fs.existsSync(filepath)) {
+                fs.unlinkSync(filepath);
+                console.log(`${bind_port} Removed : ${filepath}`);
+            }else{
+                fs.closeSync(fs.openSync(filepath, 'w'));
+                console.error(`${bind_port} Creating : ${filepath}`);
+            }
+          } catch(err) {
+            console.error(`Error : ${filepath}`);
+          } 
+    }, 1000);
+}
 
 puller.connect(pusher_address);
--- pusher.py
+++ pusher.py
@@ -1,5 +1,4 @@
 import zmq
-import time
 import sys
 
 bind_port = sys.argv[1]
@@ -9,10 +8,10 @@ socket_address = "tcp://127.0.0.1:%s" % (bind_port)
 def producer():
     context = zmq.Context()
     zmq_socket = context.socket(zmq.PUSH)
+    print("Bind to : %s" % (socket_address))
     zmq_socket.bind(socket_address)
-    print("binded to : %s" % (socket_address))
     # Start your result manager and workers before you start your producers
-    for num in range(90000):
+    for num in range(1000):
         work_message = {'num': num}
         print(num)
         zmq_socket.send_json(work_message)
